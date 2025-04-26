cb881a7ee00135a7de95e522c54bbb12ecca9728
Ahmed Adel <90573805+Dola1122@users.noreply.github.com>
2025-04-26 14:38:00 +0300

    problem 10

--- src/problems/Problem10.java
+++ src/problems/Problem10.java
@@ -10,11 +10,15 @@ import java.util.Map;
 import static java.util.Map.entry;
 
 public class Problem10 {
-    int[] states;
-    int[] startState;
-    int[] finalState;
-    char[] alphabet;
-    public Map<Integer, int[][]> transitionTable;
+    int[] states = {0, 1, 2};
+    int[] startState = {0};
+    int[] finalState = {0, 1};
+    char[] alphabet = {'x', 'y'};
+    public Map<Integer, int[][]> transitionTable = Map.ofEntries(
+            entry(0, new int[][]{{0}, {1}}),
+            entry(1, new int[][]{{0}, {2}}),
+            entry(2, new int[][]{{2}, {2}})
+    );
     NFA nfa = new NFA(states, startState, finalState, alphabet, transitionTable);
 
     public Problem10(BufferedReader br, BufferedWriter bw) throws IOException {
