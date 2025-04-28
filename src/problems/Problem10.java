f574c80b25fa6ff9471ba566b1b173286b50ce2b
Ahmed Saad <102562587+ahmedsaad123456@users.noreply.github.com>
2025-04-29 01:01:54 +0300

    complete all problems

--- src/problems/Problem10.java
+++ src/problems/Problem10.java
@@ -10,14 +10,13 @@ import java.util.Map;
 import static java.util.Map.entry;
 
 public class Problem10 {
-    int[] states = {0, 1, 2};
+    int[] states = {0, 1};
     int[] startState = {0};
     int[] finalState = {0, 1};
     char[] alphabet = {'E','x', 'y'};
     public Map<Integer, int[][]> transitionTable = Map.ofEntries(
             entry(0, new int[][]{{-1},{0}, {1}}),
-            entry(1, new int[][]{ {-1},{0}, {2}}),
-            entry(2, new int[][]{ {-1}, {2}, {2}})
+            entry(1, new int[][]{ {-1},{0}, {2}})
     );
     NFA nfa = new NFA(states, startState, finalState, alphabet, transitionTable);
 
